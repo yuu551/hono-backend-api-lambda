dfb612c7e8e8b5f6f77f80eae0ccd59f7285cacb
Yudai Jinno <jinno.yudai@classmethod.jp>
2024-09-21 00:38:13 +0900

    Initial commit

---
+++ test/hono-lambda-api-gw.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as HonoLambdaApiGw from '../lib/hono-lambda-api-gw-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/hono-lambda-api-gw-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new HonoLambdaApiGw.HonoLambdaApiGwStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
